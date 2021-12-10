46e49a29877269b7b3583b7e8bbb219178cfe724
Samantha Livengood <samanthah1129@icloud.com>
2021-12-09 19:33:44 -0500

    Resolved pattern typo in contact phone number validation

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -13,7 +13,7 @@ export const ContactForm = ({
   return (
     <form onSubmit={handleSubmit}>
       <input placeholder='Name' value={name} onChange={e => setName(e.target.value)} required />
-      <input placeholder='Phone' value={phone} onChange={e => setPhone(e.target.value)} paattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" title='Please enter a valid number in format xxx-xxx-xxxx' />
+      <input placeholder='Phone' value={phone} onChange={e => setPhone(e.target.value)} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" title='Please enter a valid number in format xxx-xxx-xxxx' />
       <input placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} type='email' />
       <button type='submit' disabled={!!foundDuplicateContact}>Add new contact</button>
       {foundDuplicateContact &&
